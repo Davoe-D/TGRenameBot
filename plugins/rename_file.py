e5a8c7b6a95de59cefbbdb5cb02950af8f8f29f3
pratik2596 <65121248+pratik2596@users.noreply.github.com>
2020-06-25 21:07:16 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -103,7 +103,10 @@ async def rename_doc(bot, update):
             else:
                 width = 0
                 height = 0
+                duration = 0
                 metadata = extractMetadata(createParser(thumb_image_path))
+                if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
                 if metadata.has("width"):
                     width = metadata.get("width")
                 if metadata.has("height"):
